da3d8de24bbdef4cf300075825a7804de3a8076f
Yogesh Kumar <yogeshkumaryk2002@gmail.com>
2025-05-07 15:51:20 +0530

    Cart logic is completed and integrated with backend and MongoDB

--- client/src/components/context/StoreContext.jsx
+++ client/src/components/context/StoreContext.jsx
@@ -20,15 +20,15 @@ const StoreContextProvider = ({ children }) => {
             setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
         }
 
-        // if (token) {
-        //     await axios.post(url+"/api/cart/add",{itemId},{headers:{token}});
-        // }
+        if (token) {
+            await axios.post(url+"/api/cart/add",{itemId},{headers:{token}});
+        }
     }
     const removeFromCart=async (itemId)=>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}));
-        // if (token) {
-        //     await axios.post(url+"/api/cart/remove",{itemId},{headers:{token}})
-        // }
+        if (token) {
+            await axios.post(url+"/api/cart/remove",{itemId},{headers:{token}})
+        }
     }
 
     const getTotalCartAmount=()=>{
@@ -48,12 +48,17 @@ const StoreContextProvider = ({ children }) => {
         setFoodList(response.data.data)
     }
 
+    const loadCartData = async (token)=>{
+        const response = await axios.post(url+"/api/cart/get",{},{headers:{token}});
+        setCartItems(response.data.cartData);
+    }
+
     useEffect(()=>{
         async function loadData(){
             await fetchFoodList();
             if (localStorage.getItem("token")) {
                 setToken(localStorage.getItem("token"));
-                // await loadCartData(localStorage.getItem("token"));
+                await loadCartData(localStorage.getItem("token"));
             }
         }
         loadData();
